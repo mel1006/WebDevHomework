24ffba3203f8dc107bd73c8b2395d484822db3b6
Melesse Perschewski <20370@nordakademie.de>
2025-11-15 22:58:52 +0100

    project structure, header and footer added

---
+++ src/components/Footer.vue
@@ -0,0 +1,74 @@
+<template>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <footer class="footer">
+        <p>© 2025 Tartu WebDev Homework. All rights reserved.</p>
+
+        <ul class="social-icons">
+            <li> <a href="#" class="fa fa-facebook"></a> </li>
+            <li> <a href="#" class="fa fa-twitter"></a> </li>
+            <li> <a href='#' class="fa fa-instagram"></a></li>
+        </ul>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style scoped>
+.footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #42b983;
+    color: white;
+    padding: 10px 20px;
+    position: fixed;
+    width: 100%;
+    bottom: 0;
+    box-sizing: border-box;
+}
+
+.social-icons {
+    display: flex;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    gap: 10px;
+}
+
+.social-icons li a {
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+    line-height: 40px;
+    text-align: center;
+    border-radius: 50%;
+    color: white;
+    font-size: 18px;
+    text-decoration: none;
+    transition: opacity 0.3s;
+}
+
+.social-icons li a:hover {
+    opacity: 0.7;
+}
+
+/* Facebook */
+.fa-facebook {
+    background: #3B5998;
+}
+
+/* Twitter */
+.fa-twitter {
+    background: #55ACEE;
+}
+
+/* Instagram */
+.fa-instagram {
+    background: #125688;
+    color: white;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,71 @@
+<template>
+    <header class="header">
+        <h3>{{ currentSite }}</h3>
+        <nav class="navbar">
+            <router-link class="router" to="/login">Login</router-link>
+            <span class="nav-separator">|</span>
+            <router-link class="router" to="/">Home</router-link>
+        </nav>
+        <div class="profile-icon">
+            <img src="@/assets/me.png" alt="Logo" />
+        </div>
+    </header>
+</template>
+
+<script>
+import { mergeProps } from 'vue';
+
+export default {
+    name: 'Header',
+    computed: {
+        currentSite() {
+            return this.$route.name ? this.$route.name.toUpperCase() : ''
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+html,
+body {
+    margin: 0;
+    padding: 0;
+}
+
+.header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 20px;
+    border-bottom: 1px solid #42b983;
+    margin: 0;
+}
+
+.navbar {
+    display: flex;
+    gap: 10px;
+}
+
+.nav-separator {
+    margin: 0 5px;
+    color: #333;
+}
+
+.router {
+    text-decoration: none;
+    padding: 5px 10px;
+    color: #42b983
+}
+
+.router:hover {
+    background-color: #b0b0b0;
+    border-radius: 3px;
+}
+
+.profile-icon img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+}
+</style>
