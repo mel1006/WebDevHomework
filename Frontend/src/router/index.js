c1320dab34a490a0a5fb2eecc1dc64da6415a1b8
Elisvi <dawn@tdl.ee>
2025-12-15 03:37:13 +0200

    Update index.js

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import auth from "../auth";
 
+
 const routes = [
   {
     path: "/",
@@ -26,6 +27,34 @@ const routes = [
     name: "login",
     component: () => import("../views/LoginView.vue"),
   },
+
+  {
+    path: "/add",
+  name: "add",
+  component: () => import("../views/AddPostView.vue"),
+  beforeEnter: async (to, from, next) => {
+    let authResult = await auth.authenticated();
+    if (!authResult) {
+      next("/login");
+    } else {
+      next();
+    }
+  },
+},
+{
+  path: "/posts/:id",
+  name: "post",
+  component: () => import("../views/PostView.vue"),
+  beforeEnter: async (to, from, next) => {
+    let authResult = await auth.authenticated();
+    if (!authResult) {
+      next("/login");
+    } else {
+      next();
+    }
+  },
+},
+  
 ];
 
 const router = createRouter({
